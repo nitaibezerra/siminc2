b2ac8c2bec9cabf3ec3da90ecec4c53e5a057e83
00764786105 <rafael.gloria@cultura.gov.br>
2017-04-27 14:24:30 -0300

    New - Importando dados de Modalidade de Pactuação

---
+++ sql/dml/Modalidade_de_Pactuacao.sql
@@ -0,0 +1,47 @@
+﻿BEGIN; -- ROLLBACK;
+
+-- DROP TABLE planointerno.tipoinstrumento;
+/*
+CREATE TABLE planointerno.tipoinstrumento
+(
+  tpiid serial NOT NULL,
+  tpidsc character varying(250) NOT NULL,
+  tpistatus character(1) DEFAULT 'A'::bpchar,
+  prsano character(4),
+  CONSTRAINT pk_planointerno_tipoinstrumento PRIMARY KEY (tpiid)
+)
+WITH (
+  OIDS=FALSE
+);
+*/
+
+SELECT
+	*
+FROM monitora.pi_categoriaapropriacao
+WHERE
+	capano = '2016'
+;
+
+--de -- SELECT * FROM planointerno.tipoinstrumento WHERE prsano = '2017'
+-- para -- SELECT * FROM monitora.pi_categoriaapropriacao
+-- DELETE FROM obras.unidadeobrasubacao; -- SELECT * FROM obras.unidadeobrasubacao;
+-- DELETE FROM monitora.pi_categoriaapropriacao;
+-- TRUNCATE TABLE planointerno.tipoinstrumento;
+BEGIN; -- ROLLBACK;
+INSERT INTO monitora.pi_categoriaapropriacao (
+	capcod,
+	capdsc,
+	capano
+)
+SELECT
+	tpiid,
+	tpidsc,
+	'2016'
+FROM planointerno.tipoinstrumento
+WHERE
+	prsano = '2017'
+;
+
+DELETE FROM monitora.pi_categoriaapropriacao WHERE capid = 278;
+
+-- COMMIT; ROLLBACK;
