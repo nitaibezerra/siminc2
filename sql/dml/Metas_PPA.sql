584611b89d0d2f7762117d1a9adf92ed103ee5b8
00764786105 <rafael.gloria@cultura.gov.br>
2017-04-27 09:50:32 -0300

    New - Cargas de Iniciativa PPA, Metas PPA, Objetivos PPA, Metas PNC e Indicadores PNC.

--- sql/dml/Metas_PPA.sql
+++ sql/dml/Metas_PPA.sql
@@ -2,29 +2,29 @@
 
 SELECT
 	*
-FROM monitora.pi_objetivo_ppa
+FROM monitora.pi_metas_ppa
 WHERE
-	oppstatus = 'A'
+	mppstatus = 'A'
 	AND prsano = '2016'
 ;
 
---de -- SELECT * FROM planointerno.ppaobjetivo WHERE prsano = '2016';
--- para -- SELECT * FROM monitora.pi_objetivo_ppa WHERE prsano = '2016';
+--de -- SELECT * FROM planointerno.metappa WHERE prsano = '2017';
+-- para -- SELECT * FROM monitora.pi_metas_ppa WHERE prsano = '2016';
 -- DELETE FROM monitora.pi_objetivo_ppa;
 -- TRUNCATE TABLE planointerno.ppaobjetivo;
 BEGIN; -- ROLLBACK;
-INSERT INTO monitora.pi_objetivo_ppa (
-	prsano,
-	oppdesc,
-	oppnome,
-	oppcod
+INSERT INTO monitora.pi_metas_ppa (
+	mppdesc,
+	mppcod,
+	mppnome,
+	prsano
 )
 SELECT
-	'2016',
-	objdescricao,
-	objnome,
-	objcod
-FROM planointerno.ppaobjetivo
+	mppdescricao,
+	mppcod,
+	mppnome,
+	'2016'
+FROM planointerno.metappa
 WHERE prsano = '2017'
 ;
 
