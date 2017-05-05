981ae66ae527159327ddc3b15b661e7b4e68c4bf
00764786105 <rafael.gloria@cultura.gov.br>
2017-05-05 16:29:42 -0300

    New - Foi desenvolvido a funcionalidade de carregar itens da opção iniciativas a partir do Objetivo PPA selecionado pelo usuário.

--- sql/dml/Iniciativa_PPA.sql
+++ sql/dml/Iniciativa_PPA.sql
@@ -1,24 +1,9 @@
 ﻿
-/*
+-- SET CLIENT_ENCODING TO 'LATIN5';
+-- SET CLIENT_ENCODING TO 'UTF-8';
 
--- DROP TABLE planointerno.ppainiciativa;
-
-CREATE TABLE planointerno.ppainiciativa
-(
-  iniid serial NOT NULL,
-  prsano character(4) NOT NULL,
-  inicod character(4) NOT NULL,
-  ininome character varying(500) NOT NULL,
-  inidescricao character varying(500),
-  inistatus character(1) NOT NULL DEFAULT 'A'::bpchar,
-  CONSTRAINT pk_ppainiciativa PRIMARY KEY (iniid),
-  CONSTRAINT ckc_inistatus_ppainici CHECK (inistatus = ANY (ARRAY['A'::bpchar, 'I'::bpchar]))
-)
-WITH (
-  OIDS=FALSE
-);
-
-*/
+-- Objetivos
+-- SELECT * FROM monitora.pi_objetivo_ppa WHERE oppcod = '1085';
 
 SELECT
 	*
@@ -27,32 +12,7 @@ WHERE
 	ippstatus = 'A'
 	AND prsano = '2016'
 ORDER BY
-ippcod
+	ippcod
 ;
-
---de -- SELECT * FROM planointerno.ppainiciativa WHERE prsano = '2017';
--- para -- SELECT * FROM monitora.pi_iniciativa_ppa WHERE prsano = '2016';
--- DELETE FROM monitora.pi_iniciativa_ppa;
--- TRUNCATE TABLE planointerno.ppainiciativa;
-BEGIN; -- ROLLBACK;
-INSERT INTO monitora.pi_iniciativa_ppa (
-	oppid,
-	ippdesc,
-	ippnome,
-	ippcod,
-	prsano
-)
-SELECT DISTINCT
-	3, -- SELECT * FROM monitora.pi_objetivo_ppa;
-	p_i.inidescricao,
-	p_i.ininome,
-	p_i.inicod,
-	'2016'
-FROM planointerno.ppainiciativa p_i -- 159 -- SELECT * FROM planointerno.ppainiciativa
---	JOIN planointerno.ppaobjetivo p_o ON p_i. -- SELECT * FROM planointerno.ppaobjetivo 
-WHERE p_i.prsano = '2017'
-;
-
-UPDATE monitora.pi_iniciativa_ppa SET ippnome = 'Não se aplica.' WHERE ippid = 135;
 
 -- COMMIT; ROLLBACK;
--- sql/dml/Objetivo_PPA.sql
+++ sql/dml/Objetivo_PPA.sql
@@ -1,12 +1,14 @@
 ﻿BEGIN; -- ROLLBACK;
 
-SELECT
-	*
-FROM monitora.pi_objetivo_ppa
-WHERE
+    SELECT
+	oppid AS codigo,
+	oppcod || '-' || oppnome AS descricao
+    FROM monitora.pi_objetivo_ppa
+    WHERE
 	oppstatus = 'A'
 	AND prsano = '2016'
-;
+    ORDER BY
+	oppcod
 
 --de -- SELECT * FROM planointerno.ppaobjetivo WHERE prsano = '2016';
 -- para -- SELECT * FROM monitora.pi_objetivo_ppa WHERE prsano = '2016';
@@ -29,5 +31,24 @@ WHERE prsano = '2017'
 ;
 
 UPDATE monitora.pi_objetivo_ppa SET oppnome = 'Não se aplica.', oppcod = '0' WHERE oppid = 11;
+
+--SELECT * FROM monitora.pi_objetivo_ppa WHERE oppcod = '1085';
+--SELECT DISTINCT oppid FROM monitora.pi_iniciativa_ppa;
+
+UPDATE monitora.pi_objetivo_ppa SET oppstatus = 'I'
+WHERE
+	oppid NOT IN(
+		SELECT DISTINCT oppid FROM monitora.pi_iniciativa_ppa UNION SELECT 11
+	)
+	OR oppid = '0'
+;
+
+SELECT * FROM monitora.pi_objetivo_ppa
+WHERE
+	oppid NOT IN(
+		SELECT DISTINCT oppid FROM monitora.pi_iniciativa_ppa UNION SELECT 11
+	)
+;
+
 
 -- COMMIT; ROLLBACK;
