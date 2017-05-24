41bfce5fb733736dbf71ba4d31061a940bb4ff07
Orion Teles de Mesquita <orion.mesquita@cultura.gov.br>
2017-05-24 12:02:46 -0300

    New - Adicionando ddls das tabelas criadas

--- sql/ddl/pi.sql
+++ sql/ddl/pi.sql
@@ -164,6 +164,201 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_cronograma TO usr_s
 
 
 
+-------------------- 24/05/2017 --------------------
+
+-- Table: planacomorc.cronograma
+
+-- DROP TABLE planacomorc.cronograma;
+
+CREATE TABLE planacomorc.cronograma
+(
+  croid serial NOT NULL, -- Chave Primária
+  crodsc character varying(200) NOT NULL, -- Descrição
+  CONSTRAINT pk_cronograma_croid PRIMARY KEY (croid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.cronograma
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.cronograma TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.cronograma TO usr_simec;
+COMMENT ON TABLE planacomorc.cronograma
+  IS 'Tabela de Cronogramas';
+COMMENT ON COLUMN planacomorc.cronograma.croid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.cronograma.crodsc IS 'Descrição';
+
+
+-- Table: planacomorc.cronograma_valor
+
+-- DROP TABLE planacomorc.cronograma_valor;
+
+CREATE TABLE planacomorc.cronograma_valor
+(
+  crvid serial NOT NULL, -- Chave Primária
+  crvdsc character varying(200) NOT NULL, -- Descrição
+  croid integer NOT NULL, -- Identificador do Cronograma
+  CONSTRAINT pk_cronograma_valor_crvid PRIMARY KEY (crvid),
+  CONSTRAINT fk_cronograma_valor_croid FOREIGN KEY (croid)
+      REFERENCES planacomorc.cronograma (croid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.cronograma_valor
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.cronograma_valor TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.cronograma_valor TO usr_simec;
+COMMENT ON TABLE planacomorc.cronograma_valor
+  IS 'Valores do Cronograma';
+COMMENT ON COLUMN planacomorc.cronograma_valor.crvid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.cronograma_valor.crvdsc IS 'Descrição';
+COMMENT ON COLUMN planacomorc.cronograma_valor.croid IS 'Identificador do Cronograma';
 
 
 
+
+-- Table: planacomorc.pi_convenio
+
+-- DROP TABLE planacomorc.pi_convenio;
+
+CREATE TABLE planacomorc.pi_convenio
+(
+  pcoid serial NOT NULL, -- Chave Primária
+  pliid integer NOT NULL, -- Plano Interno - PI
+  pcoconvenio integer NOT NULL, -- Número do Convênio
+  CONSTRAINT pk_pi_convenio_pcoid PRIMARY KEY (pcoid),
+  CONSTRAINT fk_pi_complemento_pliid FOREIGN KEY (pliid)
+      REFERENCES monitora.pi_planointerno (pliid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.pi_convenio
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.pi_convenio TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_convenio TO usr_simec;
+COMMENT ON TABLE planacomorc.pi_convenio
+  IS 'Vínculo do Plano Interno com Convênio';
+COMMENT ON COLUMN planacomorc.pi_convenio.pcoid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.pi_convenio.pliid IS 'Plano Interno - PI';
+COMMENT ON COLUMN planacomorc.pi_convenio.pcoconvenio IS 'Número do Convênio';
+
+
+-- Table: planacomorc.pi_cronograma
+
+-- DROP TABLE planacomorc.pi_cronograma;
+
+CREATE TABLE planacomorc.pi_cronograma
+(
+  pcrid serial NOT NULL, -- Chave Primária
+  pliid integer NOT NULL, -- Plano Interno - PI
+  mescod character(2) NOT NULL, -- Mês
+  pcrvalor double precision, -- Valor do Cronograma
+  crvid integer NOT NULL -- Identificador do Cronograma Valor
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.pi_cronograma
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.pi_cronograma TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_cronograma TO usr_simec;
+COMMENT ON TABLE planacomorc.pi_cronograma
+  IS 'Cronograma Físico/Financeiro do Plano Interno';
+COMMENT ON COLUMN planacomorc.pi_cronograma.pcrid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.pi_cronograma.pliid IS 'Plano Interno - PI';
+COMMENT ON COLUMN planacomorc.pi_cronograma.mescod IS 'Mês';
+COMMENT ON COLUMN planacomorc.pi_cronograma.pcrvalor IS 'Valor do Cronograma';
+COMMENT ON COLUMN planacomorc.pi_cronograma.crvid IS 'Identificador do Cronograma Valor';
+
+-- Table: planacomorc.pi_localizacao
+
+-- DROP TABLE planacomorc.pi_localizacao;
+
+CREATE TABLE planacomorc.pi_localizacao
+(
+  pilid serial NOT NULL, -- Chave Primária
+  pliid integer NOT NULL, -- Plano Interno - PI
+  estuf character(2), -- UF
+  muncod character(7), -- Código IBGE do Município
+  paiid integer, -- País
+  CONSTRAINT pk_pi_localizacao_pilid PRIMARY KEY (pilid),
+  CONSTRAINT fk_pi_complemento_pliid FOREIGN KEY (pliid)
+      REFERENCES monitora.pi_planointerno (pliid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.pi_localizacao
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.pi_localizacao TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_localizacao TO usr_simec;
+COMMENT ON TABLE planacomorc.pi_localizacao
+  IS 'Vínculo do Plano Interno com Localização do Projeto';
+COMMENT ON COLUMN planacomorc.pi_localizacao.pilid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.pi_localizacao.pliid IS 'Plano Interno - PI';
+COMMENT ON COLUMN planacomorc.pi_localizacao.estuf IS 'UF';
+COMMENT ON COLUMN planacomorc.pi_localizacao.muncod IS 'Código IBGE do Município';
+COMMENT ON COLUMN planacomorc.pi_localizacao.paiid IS 'País';
+
+
+-- Table: planacomorc.pi_responsavel
+
+-- DROP TABLE planacomorc.pi_responsavel;
+
+CREATE TABLE planacomorc.pi_responsavel
+(
+  pirid serial NOT NULL, -- Chave Primária
+  pliid integer NOT NULL, -- Plano Interno - PI
+  usucpf character(11) NOT NULL, -- CPF do usuário Responsável
+  CONSTRAINT pk_pi_responsavel_pirid PRIMARY KEY (pirid),
+  CONSTRAINT fk_pi_complemento_pliid FOREIGN KEY (pliid)
+      REFERENCES monitora.pi_planointerno (pliid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_pi_complemento_usucpf FOREIGN KEY (usucpf)
+      REFERENCES seguranca.usuario (usucpf) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.pi_responsavel
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.pi_responsavel TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_responsavel TO usr_simec;
+COMMENT ON TABLE planacomorc.pi_responsavel
+  IS 'Vínculo do Plano Interno com Usuário';
+COMMENT ON COLUMN planacomorc.pi_responsavel.pirid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.pi_responsavel.pliid IS 'Plano Interno - PI';
+COMMENT ON COLUMN planacomorc.pi_responsavel.usucpf IS 'CPF do usuário Responsável';
+
+-- Table: planacomorc.pi_sniic
+
+-- DROP TABLE planacomorc.pi_sniic;
+
+CREATE TABLE planacomorc.pi_sniic
+(
+  pisid serial NOT NULL, -- Chave Primária
+  pliid integer NOT NULL, -- Plano Interno - PI
+  pissniic integer NOT NULL, -- Número único do produto principal do sistema Mapas Culturais
+  CONSTRAINT pk_pi_sniic_pisid PRIMARY KEY (pisid),
+  CONSTRAINT fk_pi_complemento_pliid FOREIGN KEY (pliid)
+      REFERENCES monitora.pi_planointerno (pliid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planacomorc.pi_sniic
+  OWNER TO postgres;
+GRANT ALL ON TABLE planacomorc.pi_sniic TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE planacomorc.pi_sniic TO usr_simec;
+COMMENT ON TABLE planacomorc.pi_sniic
+  IS 'Vínculo do Plano Interno com SNIIC';
+COMMENT ON COLUMN planacomorc.pi_sniic.pisid IS 'Chave Primária';
+COMMENT ON COLUMN planacomorc.pi_sniic.pliid IS 'Plano Interno - PI';
+COMMENT ON COLUMN planacomorc.pi_sniic.pissniic IS 'Número único do produto principal do sistema Mapas Culturais';
