e589f0020e48e04f1d35e831286344c0b12d6af6
00764786105 <rafael.gloria@cultura.gov.br>
2017-08-28 11:17:36 -0300

    Fix - Dividindo arquivos de javascript pra facilitar a implementação nova de funcionalidade e também organizar o código.

---
+++ www/planacomorc/js/cadastro_pi_init.js
@@ -0,0 +1,303 @@
+
+    /**
+     * Ações efetuadas quando a tela de cadastro de PI é iniciada.
+     * 
+     * @returns VOID
+     */
+    function initCadastroPi(){
+        
+        var strComentarioEstadoAtual = $('#div_comentario_estado_atual').html();
+
+        if(strComentarioEstadoAtual != ""){
+            setTimeout(function() {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: false
+                };
+                toastr.success(strComentarioEstadoAtual, 'Enviado para correção');
+            }, 1300);
+        }
+
+        var tipoTransacao = $('tipotransacao').value;
+        if ('-' == tipoTransacao) {
+            // -- Desabilita o restante do formulário
+            $('plititulo').disable();
+            $('plidsc').disable();
+            $('btn_selecionar_acaptres').disable();
+        }
+
+        // Retira cor dos elementos marcados pela validação do formulário quando a validação for satisfeita(Quando o elemento for preenchido).
+        initTirarCorValidacao();
+
+        // Evento ao mudar opção de enquadramento
+        $('#unicod').change(function(){
+            carregarUG($(this).val());
+        });
+
+        $('div.div_ungcod').on('change', '#ungcod', function(){
+            carregarLimitesUnidade($(this).val());
+            carregarMetaPNC($(this).val());
+        });
+        
+        $('#orcamento').on('click', '.btnVisualizarDetalhes', function(){
+            visualizarRegistro($(this).attr('ptrid'));
+        });
+
+        $('#ungcod').change();
+        carregarSaldoFuncional();
+
+        // Evento ao mudar opção de enquadramento
+        $('#eqdid').change(function(){
+            mudarFormularioFinalistico($(this).val());
+        });
+
+        // Evento ao carregar a tela
+        mudarFormularioFinalistico($('#eqdid').val());
+        formatarTelaEnquadramentoComManutencaoItem();
+
+        // Evento ao mudar opção de Objetivos PPA
+        $('#oppid').change(function(){
+            carregarMetasPPA($(this).val(), null, $('#ungcod').val());
+            carregarIniciativaPPA($(this).val());
+        });
+
+        // Evento ao mudar opção de Metas PNC
+        $('div.div_mpnid').on('change', '#mpnid', function(){
+            carregarIndicadorPNC($(this).val());
+        });
+
+        // Evento ao mudar opção de Área Cultural
+        $('#mdeid').change(function(){
+            carregarSegmentoCultural($(this).val());
+        });
+
+        // Evento ao mudar opção de Manutenção Item
+        $('#eqdid').change(function(){
+            carregarManutencaoItem($(this).val());
+        });
+
+        // Evento ao mudar opção de Manutenção SubItem
+        $('body').on('change', '#maiid', function(){
+            carregarManutencaoSubItem($(this).val());
+            atualizarTitulo();
+        });
+        
+        // Evento ao mudar opção de Manutenção SubItem
+        $('body').on('change', '#masid', function(){
+            atualizarDescricao();
+        });
+
+        $('#btn_adicionar_sniic').click(function(){
+             var trHtml =
+                '<tr style="height: 30px;" id="tr_sniic_' + $('#input_sniic').val()+ '" >'
+                        + '<td style="text-align: left;">' + $('#input_sniic').val() + '</td>'
+                        + '<td style="text-align: center;">'
+                            + '<input type="hidden" name="lista_sniic[]" value="' + $('#input_sniic').val() + '" />'
+                            + '<span class="glyphicon glyphicon-trash link btnRemoveSniic" data-sniic="' + $('#input_sniic').val()+ '" ></span>'
+                        + '</td>'
+                + '</tr>';
+            $('#table_sniic').append(trHtml);
+            $('#input_sniic').val('');
+        });
+
+        $('#table_sniic').on('click', '.btnRemoveSniic', function(){
+            var sniic = $(this).attr('data-sniic');
+            $('#tr_sniic_'+ sniic).remove();
+        });
+
+        $('#input_sniic').keypress(function(e){
+            if(e.which == 13) {
+                $('#btn_adicionar_sniic').click();
+            }
+        });
+
+        $('#btn_selecionar_convenio').click(function(){
+            var trHtml =
+                '<tr style="height: 30px;" id="tr_convenio_' + $('#input_convenio').val()+ '" >'
+                + '<td style="text-align: left;">' + $('#input_convenio').val() + '</td>'
+                + '<td style="text-align: center;">'
+                + '<input type="hidden" name="lista_convenio[]" value="' + $('#input_convenio').val() + '" />'
+                + '<span class="glyphicon glyphicon-trash link btnRemoveConvenio" data-convenio="' + $('#input_convenio').val()+ '" ></span>'
+                + '</td>'
+                + '</tr>';
+            $('#table_convenio').append(trHtml);
+            $('#input_convenio').val('');
+        });
+
+        $('#table_convenio').on('click', '.btnRemoveConvenio', function(){
+            var convenio = $(this).attr('data-convenio');
+            $('#tr_convenio_'+ convenio).remove();
+        });
+
+        $('#input_convenio').keypress(function(e){
+            if(e.which == 13) {
+                $('#btn_selecionar_convenio').click();
+            }
+        });
+
+        $('#picedital').change(function() {
+                controlarEdital($('#picedital').is(':checked'));
+        });
+
+        $('#table_localizacao').on('click', '.btnRemoverLocalizacao', function(){
+            var id = $(this).attr('data-localizacao');
+            $('.tr_localizacao_'+ id).remove();
+        });
+
+        $('#table_localizacao_estadual').on('click', '.btnRemoverLocalizacaoEstadual', function(){
+            var id = $(this).attr('data-localizacao-estadual');
+            $('.tr_localizacao_estadual_'+ id).remove();
+        });
+
+        $('#table_localizacao_exterior').on('click', '.btnRemoverLocalizacaoExterior', function(){
+            var id = $(this).attr('data-localizacao-exterior');
+            $('.tr_localizacao_exterior_'+ id).remove();
+        });
+
+        $('#table_responsaveis').on('click', '.btnRemoverResponsaveis', function(){
+            var cpf = $(this).attr('data-responsaveis');
+            $('.tr_responsaveis_'+ cpf).remove();
+        });
+
+        // Evento ao carregar a tela
+        controlarEdital($('#picedital').is(':checked'));
+
+        $('#btn_selecionar_functional').click(function(){
+            abrir_lista();
+        });
+
+        $('#btn_selecionar_responsaveis').click(function(){
+            abrirModalResponsaveis();
+        });
+
+        $('#btn_selecionar_localizacao').click(function(){
+            abrirModalLocalizacao();
+        });
+
+        $('#btn_selecionar_localizacao_estadual').click(function(){
+            abrirModalLocalizacaoEstadual();
+        });
+
+        $('#btn_selecionar_localizacao_exterior').click(function(){
+            abrirModalLocalizacaoExterior();
+        });
+
+        $('#esfid').change(function(){
+            controlarTipoLocalizacao($(this).val());
+        });
+
+        $('#picvalorcusteio').keyup(function(){
+            this.value = mascaraglobal('###.###.###.###,##', this.value);
+            atualizarValorDoProjeto();
+            atualizarValorDetalhado();
+            atualizarValorNaoDetalhado();
+            atualizarValorLimiteDisponivelUnidade();
+            atualizarValorLimiteDisponivelFuncionalCusteio();
+            mudarCorValorProjeto();
+        });
+
+        $('#picvalorcapital').keyup(function(){
+            this.value = mascaraglobal('###.###.###.###,##', this.value);
+            atualizarValorDoProjeto();
+            atualizarValorDetalhado();
+            atualizarValorNaoDetalhado();
+            atualizarValorLimiteDisponivelUnidade();
+            atualizarValorLimiteDisponivelFuncionalCapital();
+            mudarCorValorProjeto();
+        });
+
+        $('#picquantidade').keyup(function(){
+            mudarCorCronogramaFisico();
+            avisarCronogramaFisico();
+        });
+
+        $('.input_fisico').keyup(function(){
+            mudarCorCronogramaFisico();
+            avisarCronogramaFisico();
+            atualizarTotalFisico();
+        });
+
+        $('.input_orcamentario').keyup(function(){
+            this.value = mascaraglobal('###.###.###.###,##', this.value);
+            atualizarTotalOrcamentario();
+        });
+        
+        $('.input_orcamentario.custeio').keyup(function(){
+            mudarCorCronogramaOrcamentarioCusteio();
+            avisarCronogramaOrcamentarioSuperiorCusteio();
+        });
+        
+        $('.input_orcamentario.capital').keyup(function(){
+            mudarCorCronogramaOrcamentarioCapital();
+            avisarCronogramaOrcamentarioSuperiorCapital();
+        });
+        
+        $('.input_financeiro.custeio').keyup(function(){
+            mudarCorCronogramaFinanceiroCusteio();
+            avisarCronogramaFinanceiroSuperiorCusteio();
+        });
+        
+        $('.input_financeiro.capital').keyup(function(){
+            mudarCorCronogramaFinanceiroCapital();
+            avisarCronogramaFinanceiroSuperiorCapital();
+        });
+
+        $('.input_financeiro').keyup(function(){
+            this.value = mascaraglobal('###.###.###.###,##', this.value);
+            atualizarTotalFinanceiro();
+        });
+
+        // Evento ao mudar clicar no código do PI
+        $('#span-plicod').click(function(){
+            var codPi = $('#span-plicod').html();
+            $('#span-plicod').hide();
+            $('#plicod').show().focus();
+        });
+
+        $('#pprid').change(function(){
+            formatarTelaProdutoNaoAplica($(this).val());
+        });
+
+        // Evento ao alterar o valor do código do PI
+        $('#plicod').change(function(){
+            $('#span-plicod').load('?modulo=principal/unidade/cadastro_pi&acao=A&alterarCodigoPi=ok&pliid='+$('#pliid').val() + '&plicod=' + $('#plicod').val());
+        });
+
+        // Evento ao mudar sair do campo de código do PI
+        $('#plicod').blur(function(){
+            $('#plicod').hide();
+            $('#span-plicod').show();
+        });
+
+        $('#capid').change(function(){
+
+            $.ajax({
+                url: '?modulo=principal/unidade/cadastro_pi&acao=A&verificarPactuacaoConvenio=ok&capid='+$('#capid').val(),
+                success: function($retorno){
+                    if($retorno){
+                        $('#div_siconv').show('slow');
+                    } else {
+                        $('#div_siconv').hide('slow');
+                    }
+                }
+            });
+
+        }).change();
+
+        controlarTipoLocalizacao($('#esfid').val());
+
+        if(strEsdPiCadastramento){
+            $('#formulario input, #formulario textarea, #formulario select').prop('disabled', true);
+            setTimeout($('#formulario select').prop('disabled', true).trigger("chosen:updated"), 3000);
+        }
+
+        atualizarTotalFisico();
+        atualizarTotalOrcamentario();
+        atualizarTotalFinanceiro();
+        
+        mudarCorValoresProjetosFisicoOrcamentarioFinanceiro();
+        
+        formatarTelaProdutoNaoAplica($('#pprid').val());
+    }
---
+++ www/planacomorc/js/cadastro_pi_validacoes.js
@@ -0,0 +1,257 @@
+
+    /**
+     * Controla a obrigatóriedade dos campos do formulario de acordo com o enquadramento selecionado.
+     *
+     * @returns Array
+     */
+    function definirCamposObrigatorios(){
+        var codigoEnquadramento = $('#eqdid').val();
+        var listaObrigatorios = ['plititulo', 'plidsc', 'unicod', 'ungcod','eqdid', 'neeid', 'capid', 'mdeid'];
+
+        // Se o formulário possui opções de manutenção item o sistema define como obrigatório o preenchimento dos itens de manutenção.
+        if($('#maiid option').size() > 0){
+            listaObrigatorios.push('maiid', 'masid');
+        // Se o formulario não possui as opções de manutenção item o sistema lista como obrigatório as opções Objetivo PPA, Metas PPA, Iniciativa PPA
+        } else {
+            listaObrigatorios.push('oppid', 'mppid', 'ippid', 'pprid');
+            
+            // Verifica se o usuário escolheu um produto diferente de não se aplica para verificar a validação do cronograma físico.
+            if($('#pprid').val() != intProdNaoAplica ){
+                listaObrigatorios.push('pumid', 'picquantidade');
+            }
+        }
+
+        // Se o código for diferente de Finalistico, o sistema não define como obrigatório o preenchimento das opções PNC.
+        if(codigoEnquadramento == intEnqFinalistico){
+            listaObrigatorios.push('mpnid', 'ipnid');
+        }
+
+        if($('#picedital').is(':checked')){
+            listaObrigatorios.push('mes');
+        }
+
+        return listaObrigatorios;
+    }
+
+    function submeter(pliid) {
+//        var codsubacao = $('#subacao').text();
+//        if ('' === codsubacao) {
+//            alert('O código da subação não pode ser deixado em branco.');
+//            return false;
+//        }
+
+//        O número do PI será gerado somente após a aprovação do PI pela equipe de Coordenação.
+//        var pi = document.getElementById("enquadramento").innerHTML
+//            + codsubacao
+//            + document.getElementById("nivel").innerHTML
+//            + document.getElementById("apropriacao").innerHTML
+//            + document.getElementById("codificacao").innerHTML
+//            + document.getElementById("modalidade").innerHTML;
+//        $("#plicod").val(pi);
+//
+//        var validado = true;
+//        var l = $('#plicodsubacao').val();
+//        var sub = l.length;
+//        $('plititulo').value = $('prefixotitulo').textContent + $('plititulo').value;
+
+        // Msgs personalizadas de validação.
+        addMsgCustom = new Array();
+
+        if($('input[name="ptrid"]').size() == 0){
+            $('.legend_funcional').addClass('validateRedText');
+            addMsgCustom.push('Funcional');
+        } else {
+            $('.legend_funcional').removeClass('validateRedText');
+        }
+
+        // Se não for adicionado sniic, acrescenta uma msg ao erro.
+    //        if($('[name="lista_sniic[]"]').size() == 0){
+    //            addMsgCustom.push('Números SNIIC');
+    //            $('.legend_sniic').addClass('validateRedText');
+    //        } else {
+    //            $('.legend_sniic').removeClass('validateRedText');
+    //        }
+        // Se não for adicionado convenio, acrescenta uma msg ao erro.
+    //        if($('[name="lista_convenio[]"]').size() == 0){
+    //            addMsgCustom.push('Números De Convênio');
+    //            $('.legend_convenio').addClass('validateRedText');
+    //        } else {
+    //            $('.legend_convenio').removeClass('validateRedText');
+    //        }
+
+        // Se não for selecionado nenhum tipo de localização, o sistema acrescenta uma mensagem de erro.
+        if($('#esfid').val() == ""){
+            $('.legend_localizacao').addClass('validateRedText');
+            addMsgCustom.push('Localização do Projeto');
+        } else {
+            switch($('#esfid').val()) {
+                // Verifica se a esfera é Estadual/DF.
+                case intEsfidEstadualDF:
+                    // Verifica se existe não foi inserido item na lista de localização municipal.
+                    if($('input[name="listaLocalizacaoEstadual[]"]').size() == 0){
+                        $('.legend_localizacao').addClass('validateRedText');
+                        addMsgCustom.push('Inserir Localização do Projeto Estadual/Distrito Federal');
+                    } else {
+                        $('.legend_localizacao').removeClass('validateRedText');
+                    }
+                break;
+                // Verifica se a esfera é Exterior.
+                case intEsfidExterior:
+                    // Verifica se existe não foi inserido item na lista de localização no Exterior.
+                    if($('input[name="listaLocalizacaoExterior[]"]').size() == 0){
+                        $('.legend_localizacao').addClass('validateRedText');
+                        addMsgCustom.push('Inserir Localização do Projeto no Exterior');
+                    } else {
+                        $('.legend_localizacao').removeClass('validateRedText');
+                    }
+                break;
+                // Verifica se a esfera é Municipal.
+                case intEsfidMunicipal:
+                    // Verifica se existe não foi inserido item na lista de localização municipal.
+                    if($('input[name="listaLocalizacao[]"]').size() == 0){
+                        $('.legend_localizacao').addClass('validateRedText');
+                        addMsgCustom.push('Inserir Localização do Projeto Municipal');
+                    } else {
+                        $('.legend_localizacao').removeClass('validateRedText');
+                    }
+                break;
+                default:
+                    $('.legend_localizacao').removeClass('validateRedText');
+                break;
+            }
+        }
+
+        // Se não for inserido nenhum Responsável, o sistema acrescenta uma mensagem de erro.
+        if($('#table_responsaveis input[name="listaResponsaveis[]"]').size() == 0){
+            $('.legend_responsaveis').addClass('validateRedText');
+            addMsgCustom.push('Responsáveis pelo Projeto');
+        } else {
+            $('.legend_responsaveis').removeClass('validateRedText');
+        }
+
+        // Valida se o usuário preencheu Valor do Projeto - Custeio.
+        if($('#picvalorcusteio').val() == ""){
+            $('#valor_projeto').addClass('validateRedText');
+            addMsgCustom.push('Custeio');
+        } else {
+            $('#valor_projeto').removeClass('validateRedText');
+        }
+
+        // Valida se o usuário preencheu Valor do Projeto - Capital.
+        if($('#picvalorcapital').val() == ""){
+            $('#valor_projeto').addClass('validateRedText');
+            addMsgCustom.push('Capital');
+        } else {
+            $('#picvalorcapital').removeClass('validateRedText');
+        }
+
+        /*
+         *
+         * @todo Refatorar esse código de validação da parte Custeio e Capital dividindo em funções.
+         */
+        var disponivelUnidade = textToFloat($('#td_disponivel_sub_unidade').text());
+        var disponivelFuncional = buscarValorDisponivelFuncional();
+        var valorDoProjeto = buscarValorDoProjeto();
+
+        if(valorDoProjeto > disponivelUnidade || valorDoProjeto > disponivelFuncional){
+            $('#picvalorcusteio').addClass('validateRedText');
+            $('#picvalorcapital').addClass('validateRedText');
+            $('#td_valor_projeto').addClass('validateRedText');
+            if(valorDoProjeto > disponivelUnidade){
+                addMsgCustom.push('Valor do projeto superior ao limite disponível da Sub-Unidade');
+            }
+            if(valorDoProjeto > disponivelFuncional){
+                addMsgCustom.push('Valor do projeto superior ao limite disponível da Funcional');
+            }
+        } else {
+            $('#picvalorcusteio').removeClass('validateRedText');
+            $('#picvalorcapital').removeClass('validateRedText');
+            $('#td_valor_projeto').removeClass('validateRedText');
+        }
+
+        // Verifica se o usuário escolheu um enquadramento que não tem item de manutenção para verificar a validação do cronograma físico.
+        if($('#maiid option').size() == 0){
+
+            // Verifica se o usuário escolheu um produto diferente de não se aplica para verificar a validação do cronograma físico.
+            if($('#pprid').val() != intProdNaoAplica ){
+
+                // Verifica se o cronograma físico foi preenchido.
+                if(!validarCronogramaFisicoPreenchido()){
+                    $('input.input_fisico').addClass('validateRedText');
+                    $('#td_total_fisico').addClass('validateRedText');
+                    addMsgCustom.push('Cronograma Fisíco');
+                } else {
+                    $('input.input_fisico').removeClass('validateRedText');
+                    $('#td_total_fisico').removeClass('validateRedText');
+                }
+
+                // Verifica se o valor do cronograma Físico é igual ao informado no Produto do PI.
+                if(!validarCronogramaFisicoIgualQuantidade()){
+                    $('input.input_fisico').addClass('validateRedText');
+                    $('#td_total_fisico').addClass('validateRedText');
+                    addMsgCustom.push('Soma dos valores do Cronograma Fisíco está diferente da quantidade informada para o Produto do PI');
+                } else {
+                    $('input.input_fisico').removeClass('validateRedText');
+                    $('#td_total_fisico').removeClass('validateRedText');
+                }
+            }
+
+        }
+
+        // Verifica se o cronograma orçamentário foi preenchido.
+        if(!validarCronogramaOrcamentarioPreenchido()){
+            $('input.input_orcamentario').addClass('validateRedText');
+            $('#td_total_orcamentario_custeio').addClass('validateRedText');
+            $('#td_total_orcamentario_capital').addClass('validateRedText');
+            addMsgCustom.push('Cronograma Orçamentário');
+        } else {
+            $('input.input_orcamentario').removeClass('validateRedText');
+            $('#td_total_orcamentario_custeio').removeClass('validateRedText');
+            $('#td_total_orcamentario_capital').removeClass('validateRedText');
+        }
+
+        // Verifica se o cronograma financeiro foi preenchido.
+        if(!validarCronogramaFinanceiroPreenchido()){
+            $('input.input_financeiro').addClass('validateRedText');
+            $('#td_total_financeiro').addClass('validateRedText');
+            addMsgCustom.push('Cronograma Financeiro');
+        } else {
+            $('input.input_financeiro').removeClass('validateRedText');
+            $('#td_total_financeiro').removeClass('validateRedText');
+        }
+
+        // Verifica se o valor do cronograma CUSTEIO é superior ao valor do Projeto.
+        if(!validarCronogramaOrcamentarioCusteioIgualValorProjeto()){
+            $('.input_orcamentario.custeio').addClass('validateRedText');
+            addMsgCustom.push('Soma dos valores de CUSTEIO do Cronograma Orçamentário diferente do valor de CUSTEIO do Valor do Projeto');
+        } else {
+            $('.input_orcamentario.custeio').removeClass('validateRedText');
+        }
+
+        // Verifica se o valor do cronograma CAPITAL é superior ao valor do Projeto.
+        if(!validarCronogramaOrcamentarioCapitalIgualValorProjeto()){
+            $('.input_orcamentario.capital').addClass('validateRedText');
+            addMsgCustom.push('Soma dos valores de CAPITAL do Cronograma Orçamentário diferente do valor de CAPITAL do Valor do Projeto');
+        } else {
+            $('.input_orcamentario.capital').removeClass('validateRedText');
+        }
+
+        // Verifica se o valor do cronograma CUSTEIO é superior ao valor do Projeto.
+        if(!validarCronogramaFinanceiroCusteioIgualValorProjeto()){
+            $('.input_financeiro.custeio').addClass('validateRedText');
+            addMsgCustom.push('Soma dos valores de CUSTEIO do Cronograma Financeiro diferente do valor de CUSTEIO do Valor do Projeto');
+        } else {
+            $('.input_financeiro.custeio').removeClass('validateRedText');
+        }
+
+        // Verifica se o valor do cronograma CAPITAL é superior ao valor do Projeto.
+        if(!validarCronogramaFinanceiroCapitalIgualValorProjeto()){
+            $('.input_financeiro.custeio').addClass('validateRedText');
+            addMsgCustom.push('Soma dos valores de CAPITAL do Cronograma Financeiro diferente do valor de CAPITAL do Valor do Projeto');
+        } else {
+            $('.input_financeiro.custeio').removeClass('validateRedText');
+        }
+
+        listaObrigatorios = definirCamposObrigatorios();
+        validarFormulario(listaObrigatorios, 'formulario', 'validar('+ pliid +')', addMsgCustom);
+    }
