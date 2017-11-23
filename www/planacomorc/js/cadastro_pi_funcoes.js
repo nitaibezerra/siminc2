653afd65332dbe6d2ef41a112c04683fdba02f45
00764786105 <rafael.gloria@cultura.gov.br>
2017-11-23 12:08:02 -0200

    [UPDATE][FIX] Correção de vinculação de FUNCIONAL na tela de PI normal.

--- www/planacomorc/js/cadastro_pi_funcoes.js
+++ www/planacomorc/js/cadastro_pi_funcoes.js
@@ -1317,10 +1317,14 @@
 
     /* Função para subustituir todos */
     function replaceAll(str, de, para) {
-        var pos = str.indexOf(de);
-        while (pos > -1) {
-            str = str.replace(de, para);
-            pos = str.indexOf(de);
+        if(typeof str != 'undefined'){
+            var pos = str.indexOf(de);
+            while (pos > -1) {
+                str = str.replace(de, para);
+                pos = str.indexOf(de);
+            }
+        } else {
+            str = '';
         }
         return (str);
     }
