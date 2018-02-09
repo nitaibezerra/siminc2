e7a154c905d2448f5fbab5bc3bf05c8572b5f4e9
00764786105 <rafael.gloria@cultura.gov.br>
2018-02-09 17:33:19 -0200

    [ DELETE ] Retirando arquivos desnecessários.

--- monitora/_monitora_bkp_05_02_2013/www/rodape.php
+++
@@ -1,5 +0,0 @@
-<tr><td colspan="2" height="2" bgcolor="#c0c0c0"></td></tr>
-<tr>
-   <td align="left" bgcolor="#2A4159" height="15"><font color="#FFFFFF">&nbsp;&nbsp;Data do Sistema: <?=date("d/m/Y - H:i:s")?>&nbsp;</font></td><td align="right" bgcolor="#2A4159" height="15"><font color="#FFFFFF"><?php echo SIGLA_SISTEMA; ?> - Ministério da Educação&nbsp;&nbsp; - Créditos</font></td>
-	</tr>
-</table>
