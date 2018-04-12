aca82dd77d37c308329dad89c60964a9258e6d3f
00764786105 <rafael.gloria@cultura.gov.br>
2018-04-12 18:13:22 -0300

    [ UPDATE ] Tela lista de Emendas - Melhorias. Issue #202
    Inserindo links e funcionalidade de botões e cache de filtros.

--- www/emendas/js/principal/emenda.js
+++ www/emendas/js/principal/emenda.js
@@ -28,10 +28,35 @@
             window.location.href = '?modulo=principal/emenda_form&acao=A';
         });
         
-        $('.btn-limpar').click(function(){
-            $('#requisicao').val('limpar');
-            $('#filtropi').submit();
+        $('#btn-limpar').click(function(){
+            $('#req').val('limpar');
+            $('#formFiltroEmenda').submit();
+        });
+
+        $('#btn-exportar-xls').click(function(){
+            $('#req').val('listaremendas-xls');
+            $('#formFiltroEmenda').submit();
+            $('#req').val('');
         });
         
+        $('.a_espelho').click(function(){
+            var pliid = $(this).attr('data-pi');
+            exibirEspelhoPi(pliid);
+            return false;
+        });
+
+
+    }
+
+    /**
+     * Exibe popup com Detalhes do pi. Tela de Espelho de PI.
+     *
+     * @returns VOID
+     */
+    function exibirEspelhoPi(pliid){
+        window.open(
+            '?modulo=principal/beneficiario_form&acao=A&req=espelho-pi&pliid='+ pliid,
+            'popup_espelho_pi',
+            'width=780,height=1000,status=1,menubar=1,toolbar=0,scrollbars=1,resizable=1');
     }
 
