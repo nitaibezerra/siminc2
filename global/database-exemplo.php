d8dd4d92e999d1325de183bcce42676e42bd75eb
Rafael José da Costa Gloria <rafael.gloria@cultura.gov.br>
2018-04-27 18:34:34 -0300

    [ UPDATE ] Inserindo configurações padrões.

---
+++ global/database-exemplo.php
@@ -0,0 +1,41 @@
+<?php
+
+# BASE DE DADOS DE DESENVOLVIMENTO SIMINC2
+$nome_bd = 'siminc2_desenvolvimento';
+$servidor_bd = '172.21.0.2';
+$porta_bd = '5432';
+$usuario_db = 'usr_simec';
+$senha_bd = '123456';
+
+# Base de dados SIAFI
+$servidor_bd_siafi = 'PREENCHER';
+$porta_bd_siafi = 'PREENCHER';
+$nome_bd_siafi = 'PREENCHER';
+$usuario_db_siafi = 'PREENCHER';
+$senha_bd_siafi = 'PREENCHER';
+
+# Base de dados PDDEInterativo
+$configDbPddeinterativo = new stdClass();
+$configDbPddeinterativo->host = 'PREENCHER';
+$configDbPddeinterativo->port = 'PREENCHER';
+$configDbPddeinterativo->dbname = 'PREENCHER';
+$configDbPddeinterativo->user = 'PREENCHER';
+$configDbPddeinterativo->password = 'PREENCHER';
+
+# Base de dados SIMINC1
+$configDbOldSiminc = new stdClass();
+$configDbOldSiminc->host = 'PREENCHER';
+$configDbOldSiminc->port = 'PREENCHER';
+$configDbOldSiminc->dbname = 'PREENCHER';
+$configDbOldSiminc->user = 'PREENCHER';
+$configDbOldSiminc->password = 'PREENCHER';
+$configDbOldSiminc->clientEncoding = 'ISO-8859-1';
+
+# Base de dados SIGFOR
+$configDbSigfor = new stdClass();
+$configDbSigfor->host = 'PREENCHER';
+$configDbSigfor->port = 'PREENCHER';
+$configDbSigfor->dbname = 'PREENCHER';
+$configDbSigfor->user = 'PREENCHER';
+$configDbSigfor->password = 'PREENCHER';
+
