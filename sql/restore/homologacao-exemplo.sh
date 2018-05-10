e1f9b7e29eda07d177d6849e83d25948e6aaa5b9
Rafael José da Costa Gloria <rafael.gloria@cultura.gov.br>
2018-05-10 12:31:40 -0300

    [ ADD ] Rotinas diarias de restauração de base de dados.
    Mudando estrutura de arquivos.

--- sql/restore/homologacao-exemplo.sh
+++ sql/restore/homologacao-exemplo.sh
@@ -2,20 +2,20 @@
 # Substituir dados [USUÁRIO_SERVIDOR_BKP], [IP_SERVIDOR_BKP], [IP_SERVIDOR_BANCO]
 
 # Copia o arquivo do servidor para a pasta local
-scp [USUÁRIO_SERVIDOR_BKP]@[IP_SERVIDOR_BKP]:/home/bkp_siminc/bkp_prod_dbsiminc.backup /var/www/html/siminc2/sql/restore-siminc2
+scp [USUÁRIO_SERVIDOR_BKP]@[IP_SERVIDOR_BKP]:/home/bkp_siminc/bkp_prod_dbsiminc.backup /var/www/html/siminc2/sql/restore
 
 # Criar banco siminc2_hom_new
 psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" -c "CREATE DATABASE siminc2_hom_new;"
 
 psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" -c "ALTER DATABASE siminc2_hom_new SET datestyle TO European;"
 
-pg_restore --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" --disable-triggers -O -x --verbose /var/www/html/siminc2/sql/restore-siminc2/bkp_prod_dbsiminc.backup
+pg_restore --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" --disable-triggers -O -x --verbose /var/www/html/siminc2/sql/restore/bkp_prod_dbsiminc.backup
 
 # Executar criação de estrutura de auditoria e mudança de senhas, emails
-psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" -f /var/www/html/siminc2/sql/restore-siminc2/create_auditoria.sql
+psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" -f /var/www/html/siminc2/sql/restore/create_auditoria.sql
 
 # Executar permissções nas tabelas
-psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" -f /var/www/html/siminc2/sql/restore-siminc2/grants.sql
+psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" --dbname "siminc2_hom_new" -f /var/www/html/siminc2/sql/restore/grants.sql
 
 # Deleta base de dados de backup
 psql --host [IP_SERVIDOR_BANCO] --port 5432 --username "postgres" -c "DROP DATABASE IF EXISTS siminc2_homologacao_bkp;"
