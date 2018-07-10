42bfb8321101bdf27d157e5c13fd5ec2d6d4c987
Rafael José da Costa Gloria <rafael.gloria@cultura.gov.br>
2018-07-10 12:12:07 -0300

    [FIX]Documentação Interna-Configuração
    Retirando documentação interna de bases de dados que não estão sendo utilizadas.

--- global/database_producao.dist.php
+++ global/database_producao.dist.php
@@ -1,28 +1,13 @@
 <?php
 
-# Sistema SIEMC - PRODUÇÃO
+# Base de dados do sistema SIMINC2
 $nome_bd = '';
 $servidor_bd = '';
 $porta_bd = '';
 $usuario_db = '';
 $senha_bd = '';
 
-# Sistema SIAFI - PRODUÇÃO
-$servidor_bd_siafi = '';
-$porta_bd_siafi = '';
-$nome_bd_siafi = '';
-$usuario_db_siafi = '';
-$senha_bd_siafi = '';
-
-# Sistema PDDEInterativo - PRODUÇÃO
-$configDbPddeinterativo = new stdClass();
-$configDbPddeinterativo->host = '';
-$configDbPddeinterativo->port = '';
-$configDbPddeinterativo->dbname = '';
-$configDbPddeinterativo->user = '';
-$configDbPddeinterativo->password = '';
-
-# Sistema SIGFOR
+# Base de dados do sistema SIMINC antigo
 $configDbSigfor = new stdClass();
 $configDbSigfor->host = '';
 $configDbSigfor->port = '';
